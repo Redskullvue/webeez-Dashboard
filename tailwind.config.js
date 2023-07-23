3d8de858e1be6f2df1ef344a6ff787c96f93a79b
redskullvue <pouya.9996@gmail.com>
2023-07-23 09:48:39 +0430

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
   theme: {
-    extend: {},
+    colors: {
+      // Configure your color palette here
+      green: "#14C8AB",
+      white: "#FFFFFF",
+      gray: "#F8F8F8",
+      primaryText: "#9C9C9C",
+      titleText: "#0F131B",
+      red: "#FF5358",
+      yellow: "#FF9800;",
+      blue: "#1976D2",
+    },
   },
   plugins: [],
 };
