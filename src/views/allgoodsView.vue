64465dd4ee5b7cfc8a7f89aa51b5eadbd812a3c4
redskullvue <pouya.9996@gmail.com>
2023-07-25 13:18:26 +0430

    feat : all good view added

---
+++ src/views/allgoodsView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <all-goods />
+  </div>
+</template>
+
+<script>
+import allGoods from "@/components/allgoods.vue";
+export default {
+  name: "allGoodsView",
+  components: {
+    allGoods,
+  },
+};
+</script>
