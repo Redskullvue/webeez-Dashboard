eccf6a42da40289d3e7c62374d64690e50eb2104
redskullvue <pouya.9996@gmail.com>
2023-07-25 13:18:34 +0430

    feat : All Product view added

--- src/views/ecommerceView.vue
+++ src/views/ecommerceView.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="w-full h-full">
-    <e-commerce-trainer />
+    <e-commerce-trainer v-if="this.$route.path === '/woo'" />
+    <all-goods v-if="this.$route.path === '/woo/goods'" />
   </div>
 </template>
 
 <script>
 import ECommerceTrainer from "@/components/e-commerceTrainer.vue";
+import allGoods from "@/components/allgoods.vue";
 export default {
   name: "ECommerce",
   components: {
     ECommerceTrainer,
+    allGoods,
   },
 };
 </script>
